a85b4881210b9fdd98822cbabad5ac1e7569ac2d
Mauricio Barrientos <mauribarrientos23@gmail.com>
2024-09-11 12:41:44 -0300

    DB Postgre

--- server/src/config/environments.js
+++ server/src/config/environments.js
@@ -8,7 +8,7 @@ export const environments = {
         DB_HOST: process.env.DB_HOST || 'localhost',
         DB_DIALECT: process.env.DB_DIALECT || 'postgres',
         DB_PASSWORD: process.env.DB_PASSWORD || 'root',
-        DB_USER: process.env.DB_USER || 'root',
+        DB_USER: process.env.DB_USER || 'postgres',
         DB_PORT: process.env.DB_PORT || '5432',
     },
 };
