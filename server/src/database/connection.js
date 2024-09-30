7fe2dca15c006374e307f87bc7d43e76a239f719
Fabian <fabianvillagra28@gmail.com>
2024-09-30 16:05:57 -0300

    fix: solución a la hora de crear las relaciones de la base de datos

--- server/src/database/connection.js
+++ server/src/database/connection.js
@@ -2,11 +2,11 @@ import { sequelize } from "./configDB.js";
 import relations from "../models/relations.js";
 
 export const connectDB = async () => {
+    relations();
     try {
         await sequelize.sync();
         console.log("Connected to the database");
 
-        relations();
     } catch (err) {
         console.log("Unable to connect to the database", err);
     }
