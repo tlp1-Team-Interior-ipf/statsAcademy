04977cabe6a8b7422b4e234e241825c83ce17e26
Mauricio Barrientos <mauribarrientos23@gmail.com>
2024-09-30 16:38:25 -0300

    Feat: front en vite

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,24 +1,80 @@
-import React from 'react';
-import '../styles/NavBar.css'; // Aquí incluiremos los estilos CSS
+import React, { useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
+import swal from 'sweetalert';
+import { AuthContext } from '../context/authContext'; // Contexto de autenticación
+import '../styles/NavBar.css'; // Estilos CSS
 
 const Navbar = () => {
+  const navigate = useNavigate();
+  const { user, logout } = useContext(AuthContext);
+
+  // Navegación para los botones
+  const handleSignInClick = () => {
+    navigate('/signin');
+  };
+
+  const handleLogInClick = () => {
+    navigate('/login');
+  };
+
+  const handleHomeClick = () => {
+    navigate('/');
+  };
+
+  const handleLogOutClick = async () => {
+    await logout();
+    swal({
+      title: "¡Cierre de sesión exitoso!",
+      text: "Serás redirigido a la página de inicio.",
+      icon: "success",
+      timer: 2000,
+      buttons: false,
+    });
+    setTimeout(() => {
+      navigate('/');
+    }, 2000);
+  };
+
   return (
     <nav className="navbar">
       <div className="navbar-left">
         {/* Logo */}
-        <img
-          src="" // Coloca aquí la URL del logo o usa una imagen local
-          alt="Stats Academy Logo"
-          className="navbar-logo"
-        />
+        <a href="#" className="navbar-logo" onClick={handleHomeClick}>
+          <img
+            src="img/tutorialogo.png" // URL o imagen local del logo
+            alt="Stats Academy Logo"
+            className="navbar-logo-img"
+          />
+        </a>
         {/* Nombre del sistema */}
-        <span className="navbar-brand">Stats Academy</span>
+        <span className="navbar-brand" onClick={handleHomeClick}>Stats Academy</span>
       </div>
-      
+
       <div className="navbar-right">
-        {/* Botones */}
-        <button className="btn register">Regístrate</button>
-        <button className="btn login">Iniciar sesión</button>
+        {/* Botones dependiendo del estado de autenticación */}
+        {user.isLogged ? (
+          <button
+            className="btn logout"
+            onClick={handleLogOutClick}
+          >
+            Cerrar Sesión
+          </button>
+        ) : (
+          <>
+            <button
+              className="btn register"
+              onClick={handleSignInClick}
+            >
+              Regístrate
+            </button>
+            <button
+              className="btn login"
+              onClick={handleLogInClick}
+            >
+              Iniciar sesión
+            </button>
+          </>
+        )}
       </div>
     </nav>
   );
