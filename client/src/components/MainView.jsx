8ac34ec2c2843594c46873f7d8f6113910c93855
Mauricio Barrientos <mauribarrientos23@gmail.com>
2024-10-18 16:55:22 -0300

    Feat: si

--- client/src/components/MainView.jsx
+++ client/src/components/MainView.jsx
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Carousel } from 'react-bootstrap';
-
 import '../styles/MainView.css';
 
 const MainView = () => {
+    const [isVisible, setIsVisible] = useState(false);
+
+    useEffect(() => {
+        // Retrasa la aparición para asegurarse de que la animación se ejecute correctamente
+        setTimeout(() => {
+            setIsVisible(true);
+        }, 100); // Puedes ajustar el tiempo de retraso si es necesario
+    }, []);
+
     return (
         <div className="main-view-container">
-            <div className="text-section text-section-move-down">
+            <div className={`text-section text-section-move-down fade-in ${isVisible ? 'visible' : ''}`}>
                 <h1>Stats Academy</h1>
                 <br />
                 <h2>Aprendizaje en Estadística</h2>
@@ -16,7 +24,7 @@ const MainView = () => {
                 <button className="start-button">Comienza ahora</button>
             </div>
 
-            <div className="carousel-section carousel-section-move-down">
+            <div className={`carousel-section carousel-section-move-down fade-in ${isVisible ? 'visible' : ''}`}>
                 <Carousel controls={true} indicators={true}>
                     <Carousel.Item>
                         <img
