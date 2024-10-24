6cc537e95b73102bc18d49233fa87fe48fc2b762
Fabian <fabianvillagra28@gmail.com>
2024-10-24 16:07:21 -0300

    fix: toma de id del usuario

--- client/src/components/Chat.jsx
+++ client/src/components/Chat.jsx
@@ -1,61 +1,86 @@
-import React, { useState, useEffect, useRef } from 'react';
-import swal from 'sweetalert';
 import '../styles/Chat.css';
+import React, { useState, useEffect, useRef, useContext } from 'react';
+import ReactMarkdown from 'react-markdown';
+import { AuthContext } from '../context/authContext';
 
 function App() {
   const [question, setQuestion] = useState('');
-  const [messages, setMessages] = useState([{ type: 'bot', text: 'Bienvenido! ¿En qué puedo ayudarte hoy?' }]);
-  const socket = useRef(null);
+  const [messages, setMessages] = useState([]);
+  const messagesEndRef = useRef(null);
+  const { user } = useContext(AuthContext);
+  const id = user.data.id;
 
+
+  // Recupera el historial del chat cuando se monta el componente
   useEffect(() => {
-    return () => {
-      if (socket.current) {
-        socket.current.close();
+    const fetchChatHistory = async () => {
+      try {
+        const response = await fetch(`http://localhost:4000/chat/history/${id}`, {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        });
+
+        if (response.ok) {
+          const result = await response.json();
+          const formattedMessages = result.data.map((msg) => ({
+            type: msg.sender === 'user' ? 'user' : 'bot',
+            text: msg.message,
+          }));
+          setMessages(formattedMessages);
+        } else {
+          throw new Error('Error al recuperar el historial del chat');
+        }
+      } catch (error) {
+        console.error('Error al obtener el historial del chat:', error);
       }
     };
-  }, []);
 
-  const handleSubmit = (event) => {
+    if (id) {
+      fetchChatHistory(); // Solo llama a la función si userId está disponible
+    }
+  }, [id]); // Dependencia de userId para asegurarse de que está disponible antes de hacer la solicitud
+
+  // Scroll automático al final de los mensajes
+  useEffect(() => {
+    scrollToBottom();
+  }, [messages]);
+
+  const scrollToBottom = () => {
+    messagesEndRef.current?.scrollIntoView({ behavior: 'smooth' });
+  };
+
+  const handleSubmit = async (event) => {
     event.preventDefault();
-    if (question.trim() === '') return;
+    if (question.trim() === '' || !id) return;
 
     setMessages([...messages, { type: 'user', text: question }]);
     setQuestion('');
 
-    socket.current = new WebSocket('ws://localhost:8000/ws/ask');
-
-    socket.current.onopen = () => {
-      socket.current.send(question);
-      swal({
-        title: "Mensaje enviado!",
-        text: "El mensaje se ha enviado correctamente!",
-        icon: "success",
-        timer: 1000
+    try {
+      const response = await fetch(`http://localhost:4000/chat/${id}`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ question }),
       });
-    };
 
-    let accumulatedMessage = "";
+      const data = await response.json();
 
-    socket.current.onmessage = (event) => {
-      if (event.data === 'END') {
+      if (response.ok) {
         setMessages((prevMessages) => [
           ...prevMessages,
-          { type: 'bot', text: accumulatedMessage.trim() }
+          { type: 'bot', text: data.response },
         ]);
-        socket.current.close();
       } else {
-        accumulatedMessage += event.data + " ";
+        throw new Error('Error en la respuesta');
       }
-    };
-
-    socket.current.onerror = (error) => {
-      setMessages([...messages, { type: 'bot', text: 'An error occurred' }]);
-      console.error('WebSocket error:', error);
-    };
-
-    socket.current.onclose = () => {
-      console.log('WebSocket connection closed');
-    };
+    } catch (error) {
+      console.error('Error en la petición:', error);
+      setMessages([...messages, { type: 'bot', text: 'Ocurrió un error' }]);
+    }
   };
 
   const handleKeyDown = (event) => {
@@ -66,28 +91,33 @@ function App() {
 
   return (
     <div className="Chat">
-    <header className="Chat-header">
-      <h1>Bienvenido al Tutor Inteligente</h1>
-      <div className="chat-container">
-        {messages.map((message, index) => (
-          <div key={index} className={`message ${message.type}`}>
-            {message.text}
-          </div>
-        ))}
-      </div>
-      <form className="input-form" onSubmit={handleSubmit}>
-        <input
-          type="text"
-          value={question}
-          onChange={(e) => setQuestion(e.target.value)}
-          onKeyUpCapture={handleKeyDown}
-          placeholder="Escribe aquí"
-          required
-        />
-        <button type="submit">Enviar</button>
-      </form>
-    </header>
-  </div>
+      <header className="Chat-header">
+        <h1>Bienvenido al Tutor Inteligente</h1>
+        <div className="chat-container">
+          {messages.map((message, index) => (
+            <div key={index} className={`message ${message.type}`}>
+              {message.type === 'bot' ? (
+                <ReactMarkdown>{message.text}</ReactMarkdown>
+              ) : (
+                message.text
+              )}
+            </div>
+          ))}
+          <div ref={messagesEndRef} />
+        </div>
+        <form className="input-form" onSubmit={handleSubmit}>
+          <input
+            type="text"
+            value={question}
+            onChange={(e) => setQuestion(e.target.value)}
+            onKeyUpCapture={handleKeyDown}
+            placeholder="Escribe aquí"
+            required
+          />
+          <button type="submit">Enviar</button>
+        </form>
+      </header>
+    </div>
   );
 }
 
