6aadf38c92f8dc6cc48d33b3dab8b484b579fa13
David Liva <davidliva01@gmail.com>
2024-11-12 16:52:41 -0300

    Ajustes: cambio de nombre de la carpeta con traduccioens para probar si se sube en la build

--- frontend/utils/i18next.js
+++ frontend/utils/i18next.js
@@ -1,7 +1,7 @@
 import i18next from 'i18next';
 import {initReactI18next} from 'react-i18next';
-import en from '../locales/en.json';
-import es from '../locales/es.json';
+import en from '../mistraducciones/en.json';
+import es from '../mistraducciones/es.json';
 
 export const languageResources = {
   en: {translation: en},
