a5354793ee2bc02de8daf824c34cf87ed85fceea
Mauri Barrientos <mauribarrientos23@gmail.com>
2024-12-02 15:54:51 -0300

    Fixed: tipo de dato en eval inicial

--- server/src/models/evaInicial.js
+++ server/src/models/evaInicial.js
@@ -13,7 +13,7 @@ export const EvaInicialModel = sequelize.define('EvaInicial', {
         allowNull: false,
     },
     nivel: {
-        type: DataTypes.STRING,
+        type: DataTypes.INTEGER,
         allowNull: false,
     },
 },
